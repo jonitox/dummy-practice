38b9abea0464ff065557f767083832c9844cffb5
JS <jonitox94@naver.com>
2022-04-06 15:46:01 +0900

    aamss2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,11 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> a= Arrays.asList(1,2);
-        List<Integer> b= Arrays.asList(1,2);
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(b));
+        System.out.println(a.equals(a));
     }
 }
 
